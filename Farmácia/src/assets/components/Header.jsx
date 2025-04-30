f39c66c84b8f51d1ef8c9cdf362fe2f475e65768
Nicolas C. Lima <166423644+NicolasCLima@users.noreply.github.com>
2025-04-29 22:17:36 -0300

    Add files via upload

---
+++ Farmácia/src/assets/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Header.css';
+import Logo from './Logo';
+
+
+function Header() {
+  return (
+    <header className='header'>
+        <Logo />
+            <h2 className='farma'>Farmárcia</h2>
+        <div className='buttons'>
+            <h3>Produtos</h3>
+            <h3>Sobre</h3>
+            <h3>Fale Conosco</h3>
+        </div>
+    </header>
+  )
+}
+
+export default Header
